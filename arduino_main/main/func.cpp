a9aa1b5f4392ba20529ea9f58834383cb1d8b21d
lai hung wei <128201642+s990093@users.noreply.github.com>
2024-04-28 16:17:06 +0800

    fit code

--- arduino_main/main/func.cpp
+++ arduino_main/main/func.cpp
@@ -5,13 +5,14 @@ bool DEPL;
 bool isClick;
 bool BUZT;
 
-void PostRequest() {
+void PostRequest()
+{
   HTTPClient http;
   WiFiClient client;
-  String apiUrl = "http://49.213.238.75:8000/i/local";  
+  String apiUrl = "http://49.213.238.75:8000/i/local";
   http.begin(client, apiUrl);
   http.addHeader("Content-Type", "application/json");
-  DynamicJsonDocument jsonDoc(256);  
+  DynamicJsonDocument jsonDoc(256);
   // jsonDoc["humd"] = float(SoilData);
   // jsonDoc["temp"] = float(TempData);
   // jsonDoc["elev"] = 500.0;
@@ -19,10 +20,13 @@ void PostRequest() {
   String data;
   serializeJson(jsonDoc, data);
   int httpCode = http.POST(data);
-  if (httpCode > 0) {
+  if (httpCode > 0)
+  {
     String payload = http.getString();
-  } else {
-    String temp = http.errorToString(httpCode).c_str();  
+  }
+  else
+  {
+    String temp = http.errorToString(httpCode).c_str();
     Serial.println("HTTP POST request failed. Error: " + temp);
   }
   http.end();
@@ -30,13 +34,13 @@ void PostRequest() {
 // void GetRequest() {
 //   HTTPClient http;
 //   WiFiClient client;
-//   String apiUrl = "http://49.213.238.75:8000/i/setting";  
+//   String apiUrl = "http://49.213.238.75:8000/i/setting";
 //   http.begin(client, apiUrl);
 //   http.addHeader("Content-Type", "application/json");
 //   int httpCode = http.GET();
 //   if (httpCode > 0) {
 //     String payload = http.getString();
-//     DynamicJsonDocument doc(256); 
+//     DynamicJsonDocument doc(256);
 //     DeserializationError error = deserializeJson(doc, payload);
 //     if (error) {
 //       Serial.print("JSON解析失败: ");
@@ -56,24 +60,27 @@ void PostRequest() {
 //     // Serial.println(autoDetect);
 
 //   } else {
-//     String temp = http.errorToString(httpCode).c_str(); 
+//     String temp = http.errorToString(httpCode).c_str();
 //     Serial.println("HTTP GET request failed. Error: " + temp);
 //   }
 //   http.end();
 // }
-void GetRequest() {
+void GetRequest()
+{
   HTTPClient http;
   WiFiClient client;
-  String apiUrl = "http://49.213.238.75:8000/app/";  
+  String apiUrl = "http://49.213.238.75:8000/app/";
   http.begin(client, apiUrl);
   http.setTimeout(500); // 設置超時時間為 1000 毫秒
   http.addHeader("Content-Type", "application/json");
   int httpCode = http.GET();
-  if (httpCode > 0) {
+  if (httpCode > 0)
+  {
     String payload = http.getString();
-    DynamicJsonDocument doc(256); 
+    DynamicJsonDocument doc(256);
     DeserializationError error = deserializeJson(doc, payload);
-    if (error) {
+    if (error)
+    {
       Serial.print("JSON解析失败: ");
       Serial.println(error.c_str());
       return;
@@ -81,43 +88,51 @@ void GetRequest() {
     isClick = doc["is_clicked"];
     Serial.print("isClick: ");
     Serial.println(isClick);
-  } else {
-    String temp = http.errorToString(httpCode).c_str(); 
+  }
+  else
+  {
+    String temp = http.errorToString(httpCode).c_str();
     Serial.println("HTTP GET request failed. Error: " + temp);
   }
   http.end();
 }
 
-
-void HelloWorld(){
+void HelloWorld()
+{
   Serial.println("debug test");
   digitalWrite(LED_BUILTIN, DEPL);
-  if (DEPL == 1) DEPL = 0;
-  else DEPL = 1;
+  if (DEPL == 1)
+    DEPL = 0;
+  else
+    DEPL = 1;
 }
 
-void Debounce(bool state){
+void Debounce(bool state)
+{
   static bool oldState = state;
   static bool newState;
   BUZT = 0;
   newState = state;
-  if(newState != oldState && newState == true){
+  if (newState != oldState && newState == true)
+  {
     Serial.println("彈跳！");
-    BUZT = 1 ;
+    BUZT = 1;
   }
   digitalWrite(OUT, BUZT);
   oldState = newState;
 }
 
-void Debounce2(bool state){
+void Debounce2(bool state)
+{
   static bool oldState = state;
   static bool newState;
-  int  mod = 0;
+  int mod = 0;
   newState = state;
-  if(newState != oldState){
+  if (newState != oldState)
+  {
 
     Serial.println("彈跳2！ " + newState);
-    mod = newState ;
+    mod = newState;
     digitalWrite(OUT, mod);
   }
   oldState = newState;
--- arduino_main/main/timer.h
+++ arduino_main/main/timer.h
@@ -4,9 +4,9 @@
 
 const unsigned long DebugTime = 2000;
 const unsigned long RequestTime = 5000;
-const unsigned long ServerTime = 1000; 
-const unsigned long TempTime = 1000; 
-const unsigned long BuzzTime = 155;  
+const unsigned long ServerTime = 1000;
+const unsigned long TempTime = 1000;
+const unsigned long BuzzTime = 155;
 
 #endif
 /// timer.h ///
