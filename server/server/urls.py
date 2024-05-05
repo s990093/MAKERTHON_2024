6bc936c161ff07e13fe8c98cf0cd892cb84f6d0e
lai hung wei <128201642+s990093@users.noreply.github.com>
2024-05-05 23:21:16 +0800

    feat camera

--- server/server/urls.py
+++ server/server/urls.py
@@ -23,7 +23,9 @@ from django.urls import re_path
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
+
 schema_view = get_schema_view(
+    
 openapi.Info(
       title="Snippets API",
       default_version='v1',
@@ -37,13 +39,12 @@ openapi.Info(
 )
 
 urlpatterns = [
-    path('app/', include('App.urls', namespace='App')),
-    path('test/', include('TestApp.urls', namespace='TestApp')),
+    re_path('app/', include('App.urls', namespace='App')),
+    re_path('test/', include('TestApp.urls', namespace='TestApp')),
     re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     re_path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
-    path('admin/', admin.site.urls),
-
+    re_path('admin/', admin.site.urls),
 ]
 
 if settings.DEBUG:  # 仅在开发环境中提供媒体文件
